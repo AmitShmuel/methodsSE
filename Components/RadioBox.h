80aec4350d4a3f929d93e4fd0cdc828d21396bca
edan hauon <edanhauon@gmail.com>
2017-07-28 17:08:38 +0300

    add hover to RadioBox

--- Components/RadioBox.h
+++ Components/RadioBox.h
@@ -8,14 +8,15 @@ using namespace std;
 class RadioBox : public UIComponent {
 
 	vector<string> options;
-	int selected_index;
-	short current;
+	short selected_index;
+	short hovered_index;
 
 public:
 
 	RadioBox(string* options, int len, short pos_x, short pos_y, short width, BorderType border = Solid, Color tColor = White, Color bColor = Black, UIComponent* parent = NULL);
 
 	void draw();
+	void invertNeeded(int i);
 	void mouseClicked(MOUSE_EVENT_RECORD);
 	void keyPressed(KEY_EVENT_RECORD);
 
